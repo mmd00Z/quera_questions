5ae290cc1c0b1e0a8542563611b61194f8b8b7ae
MMD <mmd.gh313@gmail.com>
2022-11-04 22:54:45 +0330

    add 2 pset solution NazarieRisman & MoadeleKhat

---
+++ CompleteSolutions/moaadele_khat.py
@@ -0,0 +1,5 @@
+# https://quera.org/problemset/147193/
+
+a , b = list(map(int, input().split()))
+
+print("infinite" if a == 0 and b == 0 else "invalid" if  a == 0 else "unique")
---
+++ CompleteSolutions/nazarie_risman.py
@@ -0,0 +1,3 @@
+# https://quera.org/problemset/83360/
+
+print("1" if input() in input() else "0")
