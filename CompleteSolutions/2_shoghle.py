1ca8f36be5b313cab29bde2d44a3f249e07b00e4
MMD <mmd.gh313@gmail.com>
2022-11-10 00:32:18 +0330

    add a PSET solution (2 Shogle) in python

---
+++ CompleteSolutions/2_shoghle.py
@@ -0,0 +1,5 @@
+# https://quera.org/problemset/111990/
+
+days = ["shanbe", "yekshanbe", "doshanbe", "seshanbe", "chaharshanbe", "panjshanbe", "jome"]
+inp = input()
+print("tatil" if inp == "jome" else "perspolis" if days.index(inp)%2 == 0 else  "bahman")
